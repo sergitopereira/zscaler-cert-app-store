eed80005b40d8d2c5c42c9f4b3e5285c2432fca0
Sergio Pereira <sergitopereira@hotmail.com>
2022-02-15 14:52:32 -0600

    version bump

--- helpers/parser.py
+++ helpers/parser.py
@@ -58,7 +58,7 @@ def plugin_selection(args):
     """
     a = UpdateCertStore()
     if args.version:
-        print('Plugin version version 1.2')
+        print('Plugin version version 1.3')
     if args.python:
         a.app_python()
     if args.git:
