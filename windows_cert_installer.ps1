03967e4c4d1cf657ea97568c999685cfb898a571
Sergio Pereira <spereira@zscaler.com>
2024-06-21 08:31:51 -0500

    Fix: ZscalerCAbundle.pem contains not only Zscaler CA but also System Trusted CAs

--- windows_cert_installer.ps1
+++ windows_cert_installer.ps1
@@ -1,11 +1,16 @@
-# Forked from https://gitlab.com/daxm/zscaler-cert-installer/-/blob/main/Zscaler_Cert_Installer_for_Windows.ps1
-
+$Folder = "$($home)\zscaler-cert-app-store"
+$Store = Join-Path -Path $Folder -ChildPath "ZscalerCAbundle.pem"
 function Create-Folder {
     ### Create Folder in user home ###
-    Write-Output "--> Change directory into users' home directory. Creating hidden forlder zscaler-cert-app-store."
-    New-Item -ItemType directory -Path '~\zscaler-cert-app-store'
-    $FILE=Get-Item '~\zscaler-cert-app-store' -Force
-    $FILE.attributes='Hidden'
+    if( -Not (Test-Path -Path $Folder ) )
+        {
+            New-Item -ItemType directory -Path $Folder
+            $FILE = Get-Item $Folder -Force
+            $FILE.attributes = 'Hidden'
+            Write-Output "Folder created  zscaler-cert-app-store on user's home profile"
+        }else{
+            Write-Output "Folder already created. Skipping"
+        }
 }
 
 function Remove-Cert-Files($filelist) {
@@ -34,87 +39,102 @@ function Get-Zscaler-Cert($certname, $pemname) {
     Export-Certificate -Cert $certpath -FilePath $path
 }
 
+function New-Bundle{
+    #if bundle exist alert it'll overwrite it
+    if( Test-Path -Path $Store -PathType Leaf) {
+        Write-Output "Certificate bundle detected. Clearing it..."
+        Clear-Content $Store
+    }
+    Write-Output "Generating certificate bundle..."
+    #getting all certs
+    #making sure
+    if ((Get-ChildItem -Path Cert:\LocalMachine\Root | Where-Object {$_.Subject -match "CN=Zscaler Root CA"})) {
+        Write-Output 'Zscaler root found.'
+        $certs=Get-ChildItem Cert: -Recurse  | Select-Object *
+        #getting certificate Authorities only
+        foreach($cert in $certs){
+            #going over Extensions
+            $ext =  $cert.Extensions
+            $ca = $false
+            foreach($e in $ext) {
+                if ($e.CertificateAuthority -eq $true){
+                    $ca = $true
+                }
+            }
+            if ($ca){
+                $line = [System.Convert]::ToBase64String($cert.RawData)
+                $issuer = "# Issuer: "+$cert.IssuerName.Name+"`n"
+                $subject = "# Subject: "+$cert.SubjectName.Name+"`n"
+                $serial = "# Serial: "+$cert.SerialNumber+"`n"
+                $thumbprint = "# Thumbprint: "+$cert.Thumbprint+"`n"
+                Add-Content -NoNewline -Path $Store -Value $issuer
+                Add-Content -NoNewline -Path $Store -Value $subject
+                Add-Content -NoNewline -Path $Store -Value $serial
+                Add-Content -NoNewline -Path $Store -Value $thumbprint
+                Add-Content -NoNewline -Path $Store -Value "-----BEGIN CERTIFICATE-----`n"
+                #pem is base 64 with new linex unix format every 64 characters
+
+                for ($i = 0; $i -lt $line.Length; $i += 64)
+                {
+                    $length = [Math]::Min(64, $line.Length - $i)
+                    $tmp = $line.SubString($i, $length)+ "`n"
+                    Add-Content -NoNewline -Path $Store -Value $tmp
+                }
+                Add-Content -NoNewline -Path $Store -Value "-----END CERTIFICATE-----`n`n"
+            }
+        }
+    }else{
+        Write-Output 'Zscaler root certificate not found on system trusted root CA store. Please make sure ZCC is installed'
+        Break
+    }
+}
+
 function Convert-Cert($certname, $penname) {
     ### Convert to PEM ###
     certutil.exe -encode $certname $pemname
 }
 
-function Add-Cert-To-Git($penname) {
+function Add-Cert-To-Git() {
     if (Get-Command "git.exe" -ErrorAction SilentlyContinue) {
         Write-Output "--> Add Zscaler cert to git cert store."
-        Get-Content $pemname | Add-Content $(git config --get http.sslcainfo)
+        Get-Content $Store | Add-Content $(git config --get http.sslcainfo)
     }
     else {
         Write-Output "git is either not installed or not in the %PATH%.  Skipping."
     }
 }
 
-function Add-Environment-Variables($location) {
+function Add-Environment-Variables {
     # Requests, PIP, NPM, Curl, Ruby. AWS
     if (-Not [System.Environment]::GetEnvironmentVariable('REQUESTS_CA_BUNDLE') ) {
-        [System.Environment]::SetEnvironmentVariable("REQUESTS_CA_BUNDLE", $location, "Machine")
+        [System.Environment]::SetEnvironmentVariable("REQUESTS_CA_BUNDLE", $Store, "Machine")
         Write-Output "REQUESTS_CA_BUNDLE Environment Variable configured"
         }
     if (-Not [System.Environment]::GetEnvironmentVariable('NODE_EXTRA_CA_CERTS') ) {
-        [System.Environment]::SetEnvironmentVariable("NODE_EXTRA_CA_CERTS", $location, "Machine")
+        [System.Environment]::SetEnvironmentVariable("NODE_EXTRA_CA_CERTS", $Store, "Machine")
         Write-Output "NODE_EXTRA_CA_CERTS Environment Variable configured"
         }
     if (-Not [System.Environment]::GetEnvironmentVariable('SSL_CERT_FILE') ) {
-        [System.Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $location, "Machine")
+        [System.Environment]::SetEnvironmentVariable("SSL_CERT_FILE", $Store, "Machine")
         Write-Output "SSL_CERT_FILE Environment Variable configured"
         }
     if (-Not [System.Environment]::GetEnvironmentVariable('AWS_CA_BUNDLE') ) {
-        [System.Environment]::SetEnvironmentVariable("AWS_CA_BUNDLE", $location, "Machine")
+        [System.Environment]::SetEnvironmentVariable("AWS_CA_BUNDLE", $Store, "Machine")
         Write-Output "AWS_CA_BUNDLE Environment Variable configured"
         }
 }
 
-function Add-Cert-To-Python($penname) {
-    if (Get-Command "python.exe" -ErrorAction SilentlyContinue) {
-        $pythonstartpath = Split-Path (Get-Command "python.exe").Path -Parent
-
-        $pythonendpath1 = '\Lib\site-packages\pip\_vendor\certifi\cacert.pem'
-        $pythonendpath2 = '\Lib\site-packages\certifi\cacert.pem'
-
-        Write-Output "--> Add Zscaler cert to python cert store."
-        $pythonpath = $pythonstartpath + $pythonendpath1
-        Get-Content $pemname | Add-Content $pythonpath
-        $pythonpath = $pythonstartpath + $pythonendpath2
-        Get-Content $pemname | Add-Content $pythonpath
-    }
-    else {
-        Write-Output "python is either not installed or not in the %PATH%.  Skipping."
-    }
-}
-
 function Main {
-    $certname = 'ZscalerRootCertificate.cer'
-    $pemname = 'ZscalerRootCertificate.pem'
-
     Create-Folder
-
-    Get-Zscaler-Cert $certname
+    New-Bundle
     Set-Location '~\zscaler-cert-app-store'
-    Convert-Cert $certname $penname
-    $File=Resolve-Path "ZscalerRootCertificate.pem"
-
-    # If cert files exist do importation to 3rd party applications.
-    if ((Test-Path -Path $certname -PathType Leaf) -And (Test-Path -Path $pemname -PathType Leaf)) {
-        Write-Output "--> Cert extraction and conversion successful."
-
-        Add-Cert-To-Git $penname
-        Add-Cert-To-Python $penname
-        Add-Environment-Variables $File
-
-    }
-    Else {
-        Write-Output "--> Extracted cert files not in expected location.  Please check home directory for files named: " + $certname + " and " + $pemname + "."
-    }
+    Add-Environment-Variables
+    Add-Cert-To-Git
 
     Write-Output "--> Program Done."
 }
 
 # Run Program
-Write-Output "Windows Zscaler Cert installer v0.2"
+Write-Output "Windows Zscaler Cert installer v1.0"
 Test-Privileges
 Main
